fcaca3178f47fdd5982872ac194b0047c3771068
GAGAR0 <zettor111@gmail.com>
2022-11-29 18:45:11 +0100

    1

---
+++ app/src/main/java/com/example/ropkoo/goalProgress2.kt
@@ -0,0 +1,11 @@
+package com.example.ropkoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class goalProgress2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_progress2)
+    }
+}
