459864e936f38f7d2e284212b778513a0e3c55ed
GAGAR0 <zettor111@gmail.com>
2022-12-13 17:34:07 +0100

    michna

--- app/src/main/java/com/example/ropkoo/calorieCalculator.kt
+++ app/src/main/java/com/example/ropkoo/calorieCalculator.kt
@@ -1,11 +1,19 @@
 package com.example.ropkoo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class calorieCalculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calorie_calculator)
+
+        var back = findViewById<ImageButton>(R.id.back)
+        back.setOnClickListener{
+            val intent = Intent(this, mainMenu::class.java)
+            startActivity(intent)
+        }
     }
 }
