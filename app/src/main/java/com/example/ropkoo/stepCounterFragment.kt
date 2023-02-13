aec64bb357429e56db33b19d9e6b191ccaa05f80
GAGAR0 <zettor111@gmail.com>
2023-02-13 19:47:06 +0100

    michna

--- app/src/main/java/com/example/ropkoo/stepCounterFragment.kt
+++ app/src/main/java/com/example/ropkoo/stepCounterFragment.kt
@@ -1,6 +1,15 @@
 package com.example.ropkoo
 
+import android.Manifest
+import android.content.Context
+import android.content.Context.SENSOR_SERVICE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,16 +17,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_step_counter.*
+import org.w3c.dom.Text
 
 
-class stepCounterFragment : Fragment() {
+/*class stepCounterFragment : Fragment() {
+
+    private var sensorManager: SensorManager? = null
+
+    // variable gives the running status
+    private var running = false
+
+    // variable counts total steps
+    private var totalSteps = 0f
+
+    val ACTIVITY_RECOGNITION_REQUEST_CODE = 100
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        sensorManager = getSystemService(SENSOR_SERVICE.Context) as SensorManager
         return inflater.inflate(R.layout.fragment_step_counter, container, false)
     }
 
@@ -29,4 +58,73 @@ class stepCounterFragment : Fragment() {
             Navigation.findNavController(view).navigate(R.id.action_stepCounterFragment_to_menuFragment)
         }
     }
+}*/
+
+class stepCounterFragment : Fragment(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+
+    // variable gives the running status
+    private var running = false
+
+    // variable counts total steps
+    private var totalSteps = 0f
+
+    val ACTIVITY_RECOGNITION_REQUEST_CODE = 100
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        return inflater.inflate(R.layout.fragment_step_counter, container, false)
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+        running = true
+
+        // TYPE_STEP_COUNTER:  A constant describing a step counter sensor
+        // Returns the number of steps taken by the user since the last reboot while activated
+        // This sensor requires permission android.permission.ACTIVITY_RECOGNITION.
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            // show toast message, if there is no sensor in the device
+            Toast.makeText(requireContext(), "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        } else {
+            // register listener with sensorManager
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        // unregister listener
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        // get textview by its id
+
+        if (running) {
+
+            //get the number of steps taken by the user.
+            totalSteps = event!!.values[0]
+
+            val currentSteps = totalSteps.toInt()
+
+            // set current steps in textview
+            stepNum.setText("$currentSteps")
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        println("onAccuracyChanged: Sensor: $sensor; accuracy: $accuracy")
+    }
+
 }
